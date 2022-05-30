658ccb80a89d29b1bb24f60f939c576a952808fb
Benjamin Bruington <Psychological_Chemits@hotmail.com>
2022-05-29 20:05:48 -0400

    new leet problem

---
+++ RemoveDuplicates.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(nums) {
+    let k = 0
+    for( i=0; i<nums.length -1; i++){
+        for( j=i+1; j<nums.length; j++){
+            if (nums[i] === nums[j] ){
+                nums.splice(j, 1);
+                
+                k++;
+            }
+        }
+    }
+    
+    return `${k} duplicates removed, nums =[${nums}]`
+};
