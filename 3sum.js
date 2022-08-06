9a0a287018ae2438e53b59e2aad7fd7bc927192b
BBruington <Psychological_Chemist@hotmail.com>
2022-08-06 16:38:33 -0400

    3sum

--- 3sum.js
+++ 3sum.js
@@ -1,18 +1,26 @@
 const threeSum = function(nums) {
   nums.sort((a,b) => a - b);
-const triplets = [];
+  //initialize the variable that'll hold all possible solutions in nums
+  const triplets = [];
 
-for(let i=0; i < nums.length - 2; i++){
+  // iterate through the array of nums looking for possible solutions
+  for(let i=0; i < nums.length - 2; i++){
    if(nums[i] != nums[i-1]){ // making sure our solution set does not contain duplicate triplets
        let left = i + 1;
      let right = nums.length - 1;
 
+        // keep checking possible sum combinations with the same i until variables left and right pass each other
        while (left < right){
+            // create a variable that'll be used to check for a solution of 0
            const currentSum = nums[i] + nums[left] + nums[right];
+
            if (currentSum === 0){
+             // if it is a solution, add it the the other solutions of triplets
                triplets.push([nums[i], nums[left], nums[right]]);
                while(nums[left] == nums[left + 1]) left ++
                while(nums[right] == nums[right - 1]) right -- // making sure our solution set does not contain duplicate triplets
+
+               // we keep checking every possible combination of the 3 sums
                left ++;
                right --;
            } else if(currentSum < 0) {
@@ -22,6 +30,8 @@ for(let i=0; i < nums.length - 2; i++){
            }
        }
    }
-}
-return triplets
+  }
+
+  //once we check every possible 3 sum, we return the variable contained every possible solution we pushed onto triplets
+  return triplets
 };
