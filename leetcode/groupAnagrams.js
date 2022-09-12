78e85c0662a7af76752ce531bdf353909d42be24
BBruington <Psychological_Chemist@hotmail.com>
2022-09-11 23:02:05 -0400

    group anagrams

---
+++ leetcode/groupAnagrams.js
@@ -0,0 +1,10 @@
+const groupAnagrams = function(strs) {
+  let hash = {}, anagrams = [];
+  
+for (let i = 0; i < strs.length; i++) {
+  let word = strs[i].split('').sort(); 
+  hash[word] = hash[word] || []; 
+  hash[word].push(strs[i]);
+}
+return Object.values(hash); 
+};
