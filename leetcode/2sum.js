ee90b0634b11c4606475ef214299ee097f7477d8
BBruington <Psychological_Chemist@hotmail.com>
2022-11-01 10:11:24 -0400

    2sum hashmap

--- leetcode/2sum.js
+++ leetcode/2sum.js
@@ -11,3 +11,25 @@ const twoSum = function(nums, target) {
   return [];
 };
 
+//hashmap
+
+var twosum = function(nums, target) {
+
+  //initialize a variable to hold possible solutions
+  let hash = {};
+
+  //iterate through the array to find possible solutions and compare them to i
+  for(let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+
+    //we subtract n from target to figure out what the possible solution is
+    //if it's already stored in our hash we return the answer
+    if(hash[target - n] !== undefined) {
+      return [hash[target - n], i];
+    }
+
+    //otherwise we store the possible solution in our hash variable
+    hash[n] = i;
+  }
+  return [];
+}
