13458122baf492d15474d86e8c6da1112b600e05
BIBruington <psychological_chemist@hotmail.com>
2023-03-21 16:56:54 -0400

    insert delete getrandom

---
+++ leetcode/insertDeleteGetrandom.js
@@ -0,0 +1,43 @@
+// Implement the RandomizedSet class:
+
+// RandomizedSet() Initializes the RandomizedSet object.
+// bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+// bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+// int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). 
+// Each element must have the same probability of being returned.
+// You must implement the functions of the class such that each function works in average O(1) time complexity.
+
+var RandomizedSet = function() {
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
