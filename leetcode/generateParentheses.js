113e06c3e0ed468b7efe00bf7df724c2e487bcab
BIBruington <psychological_chemist@hotmail.com>
2023-10-16 16:54:52 -0400

    understanding generate parentheses

--- leetcode/generateParentheses.js
+++ leetcode/generateParentheses.js
@@ -15,3 +15,25 @@ function breathFirstSearch(str, left, right) {
  }
 }
 };
+
+//ex: n = 2
+//function breathFirstSearch("", left = 0, right = 0)
+
+// left !== n? i want you to run the function again but add "(" and left +1
+
+// is left > right? i want you to run the function AGAIN but this instance has
+// ")" and right plus 1 instead
+
+//instance 1a breathFirstSearch("(", 1, 0)
+
+//instance 2a breathFirstSearch("((", 2, 0)
+//instance 2b breathFirstSearch("()", 1, 1)
+
+//instance 3a breathFirstSearch("(()", 2, 1)
+//instance 3b breathFirstSearch("()(", 2, 1)
+
+//instance 4a breathFirstSearch("(())", 2, 2)
+//if both left and right === n add it to solution
+
+//instance 4b breathFirstSearch("()()", 2, 2)
+//if both left and right === n add it to solution
