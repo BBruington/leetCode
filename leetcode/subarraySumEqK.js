84c5658cafd80d5343bd90db1460b6ce8d944065
BIBruington <psychological_chemist@hotmail.com>
2023-10-19 18:10:34 -0400

    subarraySumEqK hashmap

--- leetcode/subarraySumEqK.js
+++ leetcode/subarraySumEqK.js
@@ -4,14 +4,6 @@
 // A subarray is a contiguous non-empty sequence of elements within an array.
 
 
-// Example 1:
-
-// Input: nums = [1,1,1], k = 2
-// Output: 2
-// Example 2:
-
-// Input: nums = [1,2,3], k = 3
-// Output: 2
 
 //inefficient way
 const subarraySumSlow = function(nums, k) {
@@ -28,6 +20,32 @@ const subarraySumSlow = function(nums, k) {
       j++
     }
   }
-
+  
   return totalSums;
 };
+
+const subArraySum = function(nums, k) {
+  let hashMap = new Map();
+  let sum = 0;
+  let totalSums = 0;
+  hashMap.set(0,1);
+  for (let i=0;i<nums.length;i++) {
+    //add to sum as you move to the right
+    sum += nums[i];
+    //if the hashMap has a number that could be added to sum to equal k add a subbarray
+    if (hashMap.has(sum-k)) totalSums += hashMap.get(sum-k);
+    //if the hashMap has a value with the key sum, set it's value to itself + 1
+    if (hashMap.has(sum)) hashMap.set(sum, hashMap.get(sum)+1);
+    else hashMap.set(sum, 1);
+  }
+  return totalSums;
+}
+// Example 1:
+
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+
+// Example 2:
+
+// Input: nums = [1,2,3], k = 3
+// Output: 2
