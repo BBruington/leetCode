bd1780bc8f50673306420391d4cc28a8fe93c159
BIBruington <psychological_chemist@hotmail.com>
2023-11-08 16:36:34 -0500

    single number

--- leetcode/singleNumber.js
+++ leetcode/singleNumber.js
@@ -1,24 +1,34 @@
-const singleNumber = function(nums) {
+const singleNumber = function (nums) {
   let twos = [];
   let twosTest = [];
-  for(let i = 0; i < nums.length; i++) {
-      if( twosTest.includes(nums[i])) {
-          twos.push(nums[i])
-      }
-      else { twosTest.push(nums[i])}
+  for (let i = 0; i < nums.length; i++) {
+    if (twosTest.includes(nums[i])) {
+      twos.push(nums[i]);
+    } else {
+      twosTest.push(nums[i]);
+    }
   }
-  for(let i = 0; i < nums.length; i++) {
-      if( twos.includes(nums[i]) === false) {
-          return nums[i]
-      }
+  for (let i = 0; i < nums.length; i++) {
+    if (twos.includes(nums[i]) === false) {
+      return nums[i];
+    }
   }
   return [];
 };
 
-const singleNumberToo = function(nums) {
+const singleNumber2 = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    const result = nums.filter((num) => {
+      return num === nums[i];
+    });
+    if (result.length === 1) return result[0];
+  }
+};
+
+const singleNumber3 = function (nums) {
   res = 0;
-  for(let num of nums){
-      res^=num
+  for (let num of nums) {
+    res ^= num;
   }
   return res;
 };
