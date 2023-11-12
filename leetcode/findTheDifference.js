10f89f9335acf1c8960fa3063dc9cee50cd69e01
BIBruington <psychological_chemist@hotmail.com>
2023-11-12 18:54:41 -0500

    .

--- leetcode/findTheDifference.js
+++ leetcode/findTheDifference.js
@@ -6,5 +6,15 @@ const findTheDifference = function (s, t) {
   for (let i = 0; i < t.length; i++) {
     numForChar ^= t.charCodeAt(i);
   }
+  //the logic is that x ^= x === 0 so if you ^= 2 strings that are exactly the same except for 1 letter, you will be left with
+  //the unicode for the letter that was added then you convert it back into a string
+
+  //ex:
+  //s= "ab" t="abc"
+  // "a".charCodeAt(0) ^ "a".charCodeAt(0) ^ "b".charCodeAt(0) ^ "b".charCodeAt(0) ^ "c".charCodeAt(0)
+  // "a".charCodeAt(0) ^ "a".charCodeAt(0) = 0
+  //"b".charCodeAt(0) ^ "b".charCodeAt(0) = 0
+  //0 ^ 0 ^ "c".charCodeAt(0) = "c".charCodeAt(0)
+  // fromCharCode("c".charCodeAt(0)) = "c"
   return String.fromCharCode(numForChar);
 };
