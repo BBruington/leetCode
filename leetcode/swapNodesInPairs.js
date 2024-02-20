e859a9c761859db680b45a3fddc9a72f0e2f1c2b
BIBruington <psychological_chemist@hotmail.com>
2024-02-19 20:36:08 -0500

    swap nodes

---
+++ leetcode/swapNodesInPairs.js
@@ -0,0 +1,14 @@
+const swapPairs = (head) => {
+  const dHead = new ListNode(0, head);
+  let cur = dHead;
+  while (cur.next && cur.next.next) {
+    let first = cur.next;
+    let second = cur.next.next;
+    let temp = cur.next.next.next;
+    cur.next = second;
+    second.next = first;
+    first.next = temp;
+    cur = first;
+  }
+  return dHead.next;
+};
